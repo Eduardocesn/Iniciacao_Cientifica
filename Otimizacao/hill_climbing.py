c0c3ac3db64a9a778076aa416d2791db9fab1228
Eduardocesn <ceduardoadustina@gmail.com>
2023-09-28 23:32:35 -0300

    Realizado testes

--- Otimizacao/hill_climbing.py
+++ Otimizacao/hill_climbing.py
@@ -11,30 +11,57 @@ min = -100      #Limites dos valores
 max = 100
 d = 50          #Tamanho do vetor
 otimo = [0] * d #Vetor otimo
-
+perc = 1        #Porcentagem de variação do min e max
 
 def main():
-    a = hill_climbing(20000)
-    print(a)
-    print(np.linalg.norm(a))
+    #a = hill_climbing(100000, perc, 0, 0)
+    #print(a)
+    #print(np.linalg.norm(a))
+    grid_search()
 
+def grid_search():
+    parametros = {'raio': [1, 2, 5, 10, 20, 30, 50],
+                  'prob': [0.1, 0.25, 0.5, 0.75, 1]}
+    best_res = None
+    q_best = float(np.inf)
+    best_comb = None
+    i=0
+    for r in parametros['raio']:
+        for p in parametros['prob']:
+            res, quality = hill_climbing(100000, perc, r, p)
+            print(i, '...')
+            i+=1
+            if quality < q_best:
+                q_best = quality
+                best_res = res
+                best_comb = (r, p)
+
+    print(best_res)
+    print(q_best, best_comb)
+
+
+    
 #n: quantidade de iterações
-def hill_climbing(n):
-    S = np.random.uniform(min, max, d)
+def hill_climbing(n, perc, p, r):
+    S = np.random.uniform(min * perc, max * perc, d)
+    qS = Quality(S)
     for _ in range(n):
         #R = Tweak_one()
-        R = Tweak_two(1, 10, np.copy(S))
-        if Quality(R) < Quality(S):
+        R = Tweak_two(p, r, np.copy(S))
+        qR = Quality(R)
+        if qR < qS:
             S = R
-    return S
+            qS = qR
+    return S, qS
 
 #Função para calcular qualidade
 def Quality(a):
-    return sphere_function(a)
+    #return sphere_function(a)
+    return ackley_function(a)
 
 #Implementação do algoritmo 7 do livro Essentials of Metaheuristics
 def Tweak_one():
-    V = np.random.uniform(min, max, d)
+    V = np.random.uniform(min * perc, max * perc, d)
     return V
 
 #Implementação do algoritmo 8 do livro Essentials of Metaheuristics
